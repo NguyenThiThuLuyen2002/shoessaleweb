518ac73316c420d15f09736570fd9daa3d74237f
dothihongkhanh <dothihongkhanh22@gmail.com>
2023-11-13 00:11:19 +0700

    edit template

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -2,54 +2,81 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'THT - Trang quản trị')</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>@yield('title', 'THT - Trang quản trị')</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="/template/admin/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="/template/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="/template/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="/template/admin/dist/css/adminlte.min.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="/template/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="/template/admin/plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
-    <link rel="stylesheet" href="/template/admin/plugins/summernote/summernote-bs4.min.css">
-    @yield('style')
+   <!-- Google Font: Source Sans Pro -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="/template/admin/plugins/fontawesome-free/css/all.min.css">
+   <!-- Ionicons -->
+   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+   <!-- Tempusdominus Bootstrap 4 -->
+   <link rel="stylesheet" href="/template/admin/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+   <!-- iCheck -->
+   <link rel="stylesheet" href="/template/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+   <!-- Theme style -->
+   <link rel="stylesheet" href="/template/admin/dist/css/adminlte.min.css">
+   <!-- overlayScrollbars -->
+   <link rel="stylesheet" href="/template/admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+   <!-- Daterange picker -->
+   <link rel="stylesheet" href="/template/admin/plugins/daterangepicker/daterangepicker.css">
+   <!-- summernote -->
+   <link rel="stylesheet" href="/template/admin/plugins/summernote/summernote-bs4.min.css">
+   @yield('style')
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
+   <div class="wrapper">
+      <!-- Navbar -->
+      @include('admin.layouts.navbar')
+      <!-- End Navbar -->
 
-        <!-- Navbar -->
-        @include('admin.layouts.navbar')
-        <!-- End Navbar -->
+      <!-- Sidebar -->
+      @include('admin.layouts.sidebar')
+      <!-- End Sidebar -->
 
-        <!-- Sidebar -->
-        @include('admin.layouts.sidebar')
-        <!-- End Sidebar -->
+      <div class="content-wrapper">
 
+         <!-- Main content -->
+         <section class="content">
+            <div class="container-fluid">
 
-        <!-- Content -->
-        @yield('content')
-        <!-- End Content -->
+               @include('admin.layouts.alert')
 
-        @include('admin.layouts.footer')
+               <div class="row">
+                  <!-- left column -->
+                  <div class="col-md-12">
+                     <!-- jquery validation -->
+                     <div class="card card-primary mt-3">
+                        <div class="card-header">
+                           <h3 class="card-title">{{ $title }}</h3>
+                        </div>
+                        <div class=" mt-4">
+                           @yield('content')</div>
 
-        <aside class="control-sidebar control-sidebar-dark">
-        </aside>
-    </div>
+                     </div>
+                     <!-- /.card -->
+                  </div>
+                  <!--/.col (left) -->
+                  <!-- right column -->
+                  <div class="col-md-6">
 
-    @include('admin.layouts.javascript')
+                  </div>
+                  <!--/.col (right) -->
+               </div>
+               <!-- /.row -->
+            </div><!-- /.container-fluid -->
+         </section>
+         <!-- /.content -->
+      </div>
+
+      @include('admin.layouts.footer')
+
+   </div>
+
+   @include('admin.layouts.javascript')
 </body>
 
 </html>
