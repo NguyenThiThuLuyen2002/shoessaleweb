1cc89f9128ce7216274c03da596c1b79ed17c589
teyugn901 <thaonguyet02vn@gmail.com>
2023-11-13 23:56:48 +0700

    add navigate

---
+++ resources/client/home/home.blade.php
@@ -0,0 +1,5 @@
+<a> Đây là trang chủ </a>
+<a href="{{ route('login') }}" id="loginButton"
+class="btn btn-primary rounded-0 py-4 px-md-5 d-none d-lg-block">
+Đăng nhập<i class="fa fa-user ms-3"></i>
+</a>
