63688c89fbd0a0200eb1f16958e1a78d51942156
tluyen2602@gmail.com <tluyen2602@gmail.com>
2023-11-28 14:10:18 +0700

    update-forgotpass

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Forgotpassword</title>
+
+    <!-- Thêm link CSS của Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyJq4Ck4rGg5J7J7NQZ5Ai85v5JFKdiJSX" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-5"> <!-- Thêm class mt-5 để có khoảng cách từ đỉnh trang -->
         <form method="POST" action="{{ route('forgot.password.submit') }}">
             @csrf
 
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" required>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" id="email" required>
+            </div>
 
-            <button type="submit">Send Password Reset Link</button>
+            <button type="submit" class="btn btn-primary">Send Password Reset Link</button>
         </form>
     </div>
 </body>
--- resources/views/auth/password/reset.blade.php
+++ resources/views/auth/password/reset.blade.php
@@ -3,20 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>resetpassword</title>
+
+    <!-- Thêm link CSS của Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyJq4Ck4rGg5J7J7NQZ5Ai85v5JFKdiJSX" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
+    <div class="container mt-5">
         <form method="POST" action="{{ route('password.update') }}">
             @csrf
             <input type="hidden" name="token" value="{{ $token }}">
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="{{ $email ?? old('email') }}" required>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password" required>
-            <label for="password_confirmation">Confirm Password:</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" required>
-            <button type="submit">Reset Password</button>
+            
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" id="email" value="{{ $email ?? old('email') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" name="password" id="password" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password_confirmation">Confirm Password:</label>
+                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Reset Password</button>
         </form>
     </div>
 </body>
